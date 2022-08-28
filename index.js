16f17397fbcb431f200303f9670b585a505964da
Jeet <jeetbaldha12@gamil.com>
2022-08-28 16:59:00 +0530

    file added

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let sideMenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sideMenu.style.right="0"
+    console.log('open')
+
+}
+
+function closemenu(){
+    sideMenu.style.right="-200px";
+    console.log('close')
+    
+}
+
+let form2 = document.querySelector("form")
+form2.addEventListener('submit',function(){
+    console.log("submit")
+})
+
+// https://script.google.com/macros/s/AKfycbx8DjoW3H6TjxqroOLT0K9HUM0hyQBOfDdSCLlICG3ouxBcte11_SznoPFC5FfwL1UB/exec
+
+// https://script.google.com/macros/library/d/1uubTKMbz9hA1gdkW9T7xmbKgJ1BTAVspLeyHkRVX_eBCMkrmxj_jcY4f/1
+// contact form code
+
+
+
+
